1b4c7cf9c47e4f538b356d857d5f4e7eb57c0e97
junchanpp <9pruruluru@naver.com>
2023-02-15 12:24:32 +0900

    리눅스 환경에 맞춰 수정

--- now_escape_scrapper/scrapTool/scrapeSherlockHomes.py
+++ now_escape_scrapper/scrapTool/scrapeSherlockHomes.py
@@ -2,6 +2,7 @@ import time
 from datetime import datetime, timedelta
 from typing import Final, Dict
 
+import mysql.connector
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -26,20 +27,28 @@ SHERLOCK_HOMES_MAP: Final[Dict] = {
 
 
 def scrap_sherlock_homes_theme():
-    # connection = mysql.connector.connect(user="nowadmin", password="Nowescape1!",
-    #                                      host="nowescape-db.cvafksmpbb8z.ap-northeast-2.rds.amazonaws.com",
-    #                                      charset="utf8mb4",
-    #                                      db="now_escape")
-    # cur = connection.cursor(prepared=True)
-    now = datetime.now()
+    connection = mysql.connector.connect(user="nowadmin", password="Nowescape1!",
+                                         host="nowescape-db.cvafksmpbb8z.ap-northeast-2.rds.amazonaws.com",
+                                         charset="utf8mb4",
+                                         db="now_escape")
+    cur = connection.cursor(prepared=True)
 
     # 방탈출 매장 URL
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+
+    # linux 환경에서 필요한 option
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    # 방탈출 매장 URL
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), chrome_options=chrome_options)
     driver.get(SHERLOCK_HOMES_MAP["url"])
 
     driver.implicitly_wait(3)
 
     data_for_insert_db = []
+    now = datetime.now()
+
     for cafe in SHERLOCK_HOMES_MAP["cafeList"]:
         driver.find_element(by=By.ID, value="selectArea").find_element(by=By.XPATH,
                                                                        value=f"//option[text()='{cafe['region1ForScrap']}']").click()
@@ -61,7 +70,7 @@ def scrap_sherlock_homes_theme():
                 .find_element(by=By.XPATH, value=f"table/tbody/tr/td/a[text()='{date.day}']")
             driver.execute_script("arguments[0].click();", element)
 
-            time.sleep(2)
+            time.sleep(1)
 
             for theme in cafe["themeList"]:
                 result = driver.find_element(by=By.ID, value=theme["themeIdForScrap"])\
@@ -74,5 +83,15 @@ def scrap_sherlock_homes_theme():
 
     print(data_for_insert_db)
 
+
+    theme_id_list = []
+
+    for cafe in SHERLOCK_HOMES_MAP["cafeList"]:
+        for theme in cafe["themeList"]:
+            theme_id_list.append(theme["themeId"])
+
+    cur.execute("DELETE FROM theme_date WHERE theme_id IN (" + ','.join(str(e) for e in theme_id_list) + ")")
+    cur.executemany("INSERT IGNORE INTO theme_date(theme_time,theme_id, is_open) VALUES(?, ?, 1)", data_for_insert_db)
+    connection.commit()
 
 scrap_sherlock_homes_theme()
