a02f72e598421ee533a6267df34675976740a113
junchanpp <9pruruluru@naver.com>
2023-03-14 16:44:06 +0900

    단편선 지도 넘기는 처리 추가

--- src/scrapers/danPyeonSeon/scrapeDanPyeonSeon.py
+++ src/scrapers/danPyeonSeon/scrapeDanPyeonSeon.py
@@ -22,12 +22,11 @@ def scrap_dps_theme():
     for dateDelta in range(7):
         date = now + timedelta(dateDelta)
         date_str = date.strftime('%Y-%-m-%-d')
+        click_date(driver, date)
 
         for theme in DPS_THEME_LIST:
             theme_time_result = get_theme_time_result(driver, date_str, theme.theme_name)
             time_list = [element.text for element in theme_time_result]
-            #요기서 자르면 되지 않을까
-            print(time_list)
             theme_date_list = theme_date_list + (make_theme_date(theme.theme_id, date_str, time_list))
             print(f'{datetime.now()} scarping {date_str} {theme.theme_name}')
 
@@ -36,10 +35,18 @@ def scrap_dps_theme():
     update_theme_date(theme_id_list, theme_date_list)
 
 
+@try_except_handling
+def click_date(driver, date):
+    calendar_text = \
+        driver.find_element(by=By.XPATH, value=f'//*[@id="w20220702d6098845a46a0"]/div/div/div[1]/div/p').text
+    if calendar_text != date.strftime('%Y년 %m월'):
+        driver.find_element(by=By.XPATH, value=f'//*[@id="w20220702d6098845a46a0"]/div/div/div[1]/div/a[2]').click()
+
+
 def get_theme_time_result(driver, date_str, theme_name):
-    return driver.find_elements\
-     (by=By.XPATH, value=f"//td[contains(@data-date,'{date_str}')]/div/div[3]/"
-        f"div[contains(@class, 'booking_list  hide_badge')]/a/div/div/span[1][contains(text(), '{theme_name}')]").copy()
+    return driver.find_elements \
+        (by=By.XPATH, value=f"//td[contains(@data-date,'{date_str}')]/div/div[3]/"
+                            f"div[contains(@class, 'booking_list  hide_badge')]/a/div/div/span[1][contains(text(), '{theme_name}')]").copy()
 
 
 scrap_dps_theme()
