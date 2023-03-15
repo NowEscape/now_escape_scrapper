5706b580f14dc3613594c04149584cdf6925b54b
junchanpp <9pruruluru@naver.com>
2023-03-16 00:11:27 +0900

    덤앤더머 리팩토링

--- src/scrapers/dumbNdumber/scrapeDumbNdumber.py
+++ src/scrapers/dumbNdumber/scrapeDumbNdumber.py
@@ -1,5 +1,8 @@
 import urllib
 from datetime import datetime, timedelta
+
+import bs4
+
 from src.scrapers.dumbNdumber.constant import DUMB_N_DUMBER_CAFE_LIST
 from src.utils.database import make_theme_date, update_theme_date
 from bs4 import BeautifulSoup
@@ -17,9 +20,12 @@ def scrape_dumb_n_dumber_theme():
         date_str = date.strftime('%Y-%m-%d')
 
         for cafe in DUMB_N_DUMBER_CAFE_LIST:
+            url = urllib.request.Request(cafe.url + date_str)
+
+            html = urllib.request.urlopen(url).read().decode("utf-8")
+            bs_object = BeautifulSoup(html, "lxml")
             for theme in cafe.theme_list:
-                time_list = scrape_dumb_n_dumber_theme_bs4(date_str, cafe.url, theme.theme_num)
-                print(time_list)
+                time_list = scrape_dumb_n_dumber_theme_bs4(bs_object, theme.theme_num)
                 theme_date_list = theme_date_list + (make_theme_date(theme.theme_id, date_str, time_list))
                 print(f'{datetime.now()} scraping {date_str} {cafe.location} {theme.theme_name}')
 
@@ -29,12 +35,7 @@ def scrape_dumb_n_dumber_theme():
 
 
 @try_except_handling
-def scrape_dumb_n_dumber_theme_bs4(date: str, raw_url: str, theme_num: int):
-    url_final = raw_url + date
-    url = urllib.request.Request(url_final)
-
-    html = urllib.request.urlopen(url).read().decode("utf-8")
-    bs_object = BeautifulSoup(html, "lxml")
+def scrape_dumb_n_dumber_theme_bs4(bs_object: bs4.BeautifulSoup, theme_num: int):
     return [element.get_text().strip() for element in bs_object.select(
         f'#container > div > div > div:nth-child({theme_num}) > div.time_Area > ul > li > a:not(.end) > span.time')]
 
