b21c7377ca09ceb6f93699d457c78d9d878bef81
junchanpp <9pruruluru@naver.com>
2023-03-15 15:05:08 +0900

    이스케이퍼스 상수 추가

---
+++ src/scrapers/escapers/constant.py
@@ -0,0 +1,11 @@
+ESCAPERS_URL = "https://escapersd.com/reservation/theme"
+
+class EscapersTheme:
+    def __init__(self, themePK: int, theme_id: int):
+        self.themePK = themePK
+        self.theme_id = theme_id
+
+class EscapersCafe:
+    def __init__(self, name: str, theme_list: list[EscapersTheme]):
+        self.name = name
+        self.theme_list = theme_list
