a4464d6c62561aefa97c5edc9bb457c67fcc6a46
kye8524 <cjy7686@naver.com>
2023-03-16 02:07:22 +0900

    시크릿코드 스크래핑 코드 추가 처리필요

--- src/scrapers/secretCode/scrapeSecretCode.py
+++ src/scrapers/secretCode/scrapeSecretCode.py
@@ -18,7 +18,7 @@ def scrape_secret_code_theme():
 
         for cafe in SECRET_CODE_CAFE_LIST:
             for theme in cafe.theme_list:
-                time_list = scrape_secret_code_theme_bs4(date_str, cafe.url, theme.JIJEM, theme.theme_num)
+                time_list = scrape_secret_code_theme_bs4(date_str, theme.url)
                 print(time_list)
                 theme_date_list = theme_date_list + (make_theme_date(theme.theme_id, date_str, time_list))
                 print(f'{datetime.now()} scraping {date_str} {theme.theme_name}')
@@ -28,17 +28,16 @@ def scrape_secret_code_theme():
     update_theme_date(theme_id_list, theme_date_list)
 
 
-
-def scrape_secret_code_theme_bs4(date: str, raw_url: str, jijem: str, theme_num: int):
-    details = urllib.parse.urlencode({'JIJEM': jijem, 'D_ROOM': "", 'H_Date': date})
-    details = details.encode('UTF-8')
-    url = urllib.request.Request(raw_url, details)
+@try_except_handling
+def scrape_secret_code_theme_bs4(date: str, raw_url: str):
+    url_final = raw_url + date
+    url = urllib.request.Request(url_final)
 
     html = urllib.request.urlopen(url).read()
     bs_object = BeautifulSoup(html, "lxml")
     return [element.get_text().strip() for element in bs_object.select
-                         (f'#sub_content3 > div:nth-child({theme_num}) > ul > a > li > span.time[style="color: #FFF"]')]
+                                                    ('#sub_content3 > div > ul > li > span.time[style="color: #FFF"]')]
 
-#sub_content3 > div:nth-child({theme_num}) > ul > a:nth-child(2) > li > span.time
+
 if __name__ == '__main__':
     scrape_secret_code_theme()
