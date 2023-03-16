3bb2805a4150890a4b03008c0ed744bab19dc3ab
9pruruluru2 <9pruruluru@naver.com>
2023-03-17 03:19:42 +0900

    마스터키 완료

---
+++ src/scrapers/masterKey/constant.py
@@ -0,0 +1,41 @@
+MASTER_KEY_URL = "http://www.master-key.co.kr/booking/booking_list_new"
+
+
+class MasterKeyTheme:
+    def __init__(self, name: str, theme_id: int):
+        self.name = name
+        self.theme_id = theme_id
+
+
+class MasterKeyCafe:
+
+    def __init__(self, name: str, store_id: int, theme_list: list[MasterKeyTheme]):
+        self.name = name
+        self.store_id = store_id
+        self.theme_list = theme_list
+
+
+MASTER_KEY_CAFE_LIST = [
+    MasterKeyCafe(
+        name="궁동직영점",
+        store_id=1,
+        theme_list=[
+            MasterKeyTheme(name="MSI 미제사건 전담반", theme_id=1141),
+            MasterKeyTheme(name="재심", theme_id=1140),
+            MasterKeyTheme(name="MK빌딩을털어라:45C6", theme_id=1142),
+            MasterKeyTheme(name="블랙룸", theme_id=1138),
+            MasterKeyTheme(name="화이트룸", theme_id=1139)
+        ]
+    ),
+    MasterKeyCafe(
+        name="은행직영점",
+        store_id=2,
+        theme_list=[
+            MasterKeyTheme(name="그레이트월", theme_id=1137),
+            MasterKeyTheme(name="편지", theme_id=1135),
+            # MasterKeyTheme(name="1990", theme_id=), db에 없는 테마
+            MasterKeyTheme(name="대탈출2", theme_id=1134),
+            MasterKeyTheme(name="REC", theme_id=1136),
+        ]
+    ),
+]
---
+++ src/scrapers/masterKey/scrapeMasterKey.py
@@ -0,0 +1,45 @@
+import urllib.request
+from datetime import datetime, timedelta
+
+from bs4 import BeautifulSoup
+
+from src.scrapers.masterKey.constant import MASTER_KEY_CAFE_LIST, MASTER_KEY_URL
+from src.utils.database import make_theme_date, update_theme_date
+from src.utils.dateUtil import get_time_str
+
+
+def scrape_master_key():
+    theme_date_list = []
+    now = datetime.now()
+
+    for dateDelta in range(7):
+        date = now + timedelta(dateDelta)
+        date_str = date.strftime('%Y-%m-%d')
+
+        for cafe in MASTER_KEY_CAFE_LIST:
+            bs_object = get_bs_object(MASTER_KEY_URL, cafe.store_id, date_str)
+
+            for theme in cafe.theme_list:
+                time_list = scrape_master_key_bs4(bs_object, theme)
+                theme_date_list = theme_date_list + (make_theme_date(theme.theme_id, date_str, time_list))
+                print(f'{datetime.now()} scraping {date_str} {cafe.name} {theme.name}')
+
+    theme_id_list = [theme.theme_id for cafe in MASTER_KEY_CAFE_LIST for theme in cafe.theme_list]
+
+    update_theme_date(theme_id_list, theme_date_list)
+
+
+def get_bs_object(url, store_id, date):
+    details = urllib.parse.urlencode({'date': date, 'store': store_id})
+    details = details.encode('UTF-8')
+    html = urllib.request.urlopen(url, details).read()
+    return BeautifulSoup(html, "lxml")
+
+
+def scrape_master_key_bs4(bs_object, theme):
+    return [get_time_str(time_element.get_text())
+            for time_element in bs_object.select(f'div.box2-inner:has(div.title:-soup-contains("{theme.name}")) p.true a')]
+
+
+if __name__ == '__main__':
+    scrape_master_key()
