f7372dafb91f6718161b0519c357fd864b33db31
9pruruluru2 <9pruruluru2@naver.com>
2023-03-17 14:10:23 +0900

    version up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='now_escape_scarping',
-  version='1.0.0',
+  version='1.0.1',
   description='방탈출 예약 가능한 시간대 스크래핑 자동화 툴',
   author='nowescaperoom',
   author_email='nowescaperoom1@gmail.com',
