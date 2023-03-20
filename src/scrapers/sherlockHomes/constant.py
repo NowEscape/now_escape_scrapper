252bd0c7d69e3674982c1db918b8907f1fab5390
kye8524 <cjy7686@naver.com>
2023-03-20 17:26:42 +0900

    셜록홈즈 bs4로 수정중

--- src/scrapers/sherlockHomes/constant.py
+++ src/scrapers/sherlockHomes/constant.py
@@ -1,6 +1,6 @@
 from typing import Final
 
-SHERLOCK_URL: Final = "https://sherlock-holmes.co.kr/reservation"
+SHERLOCK_HOMES_URL: Final = "https://sherlock-holmes.co.kr/reservation/res_schedule.php?sido={SIDO}&bno={BNO}&date={DATE}&tno="
 
 
 class SherlockHomesTheme:
@@ -11,9 +11,11 @@ class SherlockHomesTheme:
 
 
 class SherlockHomesCafe:
-    def __init__(self, region1: str, region2: str, theme_list: list[SherlockHomesTheme]):
+    def __init__(self, region1: str, region2: str, sido: int, bno: int, theme_list: list[SherlockHomesTheme]):
         self.region1 = region1
         self.region2 = region2
+        self.sido = sido
+        self.bno = bno
         self.theme_list = theme_list
 
 
@@ -21,6 +23,8 @@ SHERLOCK_HOMES_CAFE_LIST: Final = [
     SherlockHomesCafe(
         region1="서울특별시",
         region2="노원점",
+        sido=1,
+        bno=57,
         theme_list=[
             SherlockHomesTheme(theme_name="황금 감옥 : 와캄", theme_id=548),
             SherlockHomesTheme(theme_name="지프리트의 심장", theme_id=549),
@@ -30,10 +34,11 @@ SHERLOCK_HOMES_CAFE_LIST: Final = [
             SherlockHomesTheme(theme_name="미녀와야수2", theme_id=546),
         ],
     ),
-    # 서을 특별시 노량진점 추가
     SherlockHomesCafe(
         region1="서울특별시",
         region2="노량진점",
+        sido=1,
+        bno=48,
         theme_list=[
             SherlockHomesTheme(theme_name="어린왕자", theme_id=636),
             SherlockHomesTheme(theme_name="빛과 그림자:화이트의죽음2", theme_id=637),
@@ -44,8 +49,9 @@ SHERLOCK_HOMES_CAFE_LIST: Final = [
     SherlockHomesCafe(
         region1="서울특별시",
         region2="대학로점",
+        sido=1,
+        bno=88,
         theme_list=[
-
             SherlockHomesTheme(theme_name="미녀와야수(대학로)", theme_id=507),
             SherlockHomesTheme(theme_name="드림 인베이더(대학로ver)", theme_id=510),
             SherlockHomesTheme(theme_name="혼:원혼의저주 (대학로)", theme_id=511),
@@ -56,6 +62,8 @@ SHERLOCK_HOMES_CAFE_LIST: Final = [
     SherlockHomesCafe(
         region1="서울특별시",
         region2="성신여대점",
+        sido=1,
+        bno=54,
         theme_list=[
             SherlockHomesTheme(theme_name="호텔:사라진 다이아몬드", theme_id=523),
             SherlockHomesTheme(theme_name="크라임씬", theme_id=524),
@@ -67,6 +75,8 @@ SHERLOCK_HOMES_CAFE_LIST: Final = [
     SherlockHomesCafe(
         region1="서울특별시",
         region2="신림2호점",
+        sido=1,
+        bno=77,
         theme_list=[
             SherlockHomesTheme(theme_name="무속인 살인사건", theme_id=591),
             SherlockHomesTheme(theme_name="미녀와야수", theme_id=594),
@@ -77,6 +87,8 @@ SHERLOCK_HOMES_CAFE_LIST: Final = [
     SherlockHomesCafe(
         region1="서울특별시",
         region2="잠실1호점",
+        sido=1,
+        bno=35,
         theme_list=[
             SherlockHomesTheme(theme_name="무속인 살인사건", theme_id=246),
             SherlockHomesTheme(theme_name="빛과 그림자", theme_id=247),
@@ -90,6 +102,8 @@ SHERLOCK_HOMES_CAFE_LIST: Final = [
     SherlockHomesCafe(
         region1="서울특별시",
         region2="잠실2호점",
+        sido=1,
+        bno=67,
         theme_list=[
             SherlockHomesTheme(theme_name="랑슈 뷰티연구소", theme_id=253),
             SherlockHomesTheme(theme_name="단잠", theme_id=254),
@@ -99,6 +113,8 @@ SHERLOCK_HOMES_CAFE_LIST: Final = [
     SherlockHomesCafe(
         region1="서울특별시",
         region2="종각점",
+        sido=1,
+        bno=69,
         theme_list=[
             SherlockHomesTheme(theme_name="숙제", theme_id=563),
             SherlockHomesTheme(theme_name="호텔:사라진 다이아몬드", theme_id=564),
@@ -110,6 +126,8 @@ SHERLOCK_HOMES_CAFE_LIST: Final = [
     SherlockHomesCafe(
         region1="경기도",
         region2="구리점",
+        sido=9,
+        bno=41,
         theme_list=[
             SherlockHomesTheme(theme_name="호텔:사라진 다이아몬드", theme_id=860),
             SherlockHomesTheme(theme_name="무속인 살인사건", theme_id=861),
@@ -121,6 +139,8 @@ SHERLOCK_HOMES_CAFE_LIST: Final = [
     SherlockHomesCafe(
         region1="경기도",
         region2="뉴안양점",
+        sido=9,
+        bno=81,
         theme_list=[
             SherlockHomesTheme(theme_name="웨딩크루즈 살인사건-2탄", theme_id=663),
             SherlockHomesTheme(theme_name="파노라마", theme_id=664),
@@ -133,6 +153,8 @@ SHERLOCK_HOMES_CAFE_LIST: Final = [
     SherlockHomesCafe(
         region1="경기도",
         region2="동탄점",
+        sido=9,
+        bno=79,
         theme_list=[
             SherlockHomesTheme(theme_name="던전을 부탁해", theme_id=781),
             SherlockHomesTheme(theme_name="좀비여친", theme_id=782),
@@ -145,6 +167,8 @@ SHERLOCK_HOMES_CAFE_LIST: Final = [
     SherlockHomesCafe(
         region1="경기도",
         region2="부천점",
+        sido=9,
+        bno=64,
         theme_list=[
             SherlockHomesTheme(theme_name="프로젝트:노아", theme_id=973),
             SherlockHomesTheme(theme_name="사각사각", theme_id=974),
@@ -155,6 +179,8 @@ SHERLOCK_HOMES_CAFE_LIST: Final = [
     SherlockHomesCafe(
         region1="경기도",
         region2="분당야탑점",
+        sido=9,
+        bno=62,
         theme_list=[
             SherlockHomesTheme(theme_name="빛과 그림자:화이트의죽음2", theme_id=832),
             SherlockHomesTheme(theme_name="혼:원혼의저주", theme_id=833),
@@ -166,6 +192,8 @@ SHERLOCK_HOMES_CAFE_LIST: Final = [
     SherlockHomesCafe(
         region1="경기도",
         region2="서현점",
+        sido=9,
+        bno=87,
         theme_list=[
             SherlockHomesTheme(theme_name="Molaq : 성 밖 이야기", theme_id=837),
             SherlockHomesTheme(theme_name="Below : 성 안 이야기", theme_id=838),
@@ -175,6 +203,8 @@ SHERLOCK_HOMES_CAFE_LIST: Final = [
     SherlockHomesCafe(
         region1="경기도",
         region2="의정부점",
+        sido=9,
+        bno=61,
         theme_list=[
             SherlockHomesTheme(theme_name="프로젝트:노아", theme_id=885),
             SherlockHomesTheme(theme_name="노량진 고시생의 사랑-여자편", theme_id=886),
@@ -186,6 +216,8 @@ SHERLOCK_HOMES_CAFE_LIST: Final = [
     SherlockHomesCafe(
         region1="경기도",
         region2="평택점",
+        sido=9,
+        bno=80,
         theme_list=[
             SherlockHomesTheme(theme_name="반고흐의방", theme_id=807),
             SherlockHomesTheme(theme_name="혼:원혼의저주", theme_id=808),
@@ -197,6 +229,8 @@ SHERLOCK_HOMES_CAFE_LIST: Final = [
     SherlockHomesCafe(
         region1="인천광역시",
         region2="구월인천점",
+        sido=4,
+        bno=13,
         theme_list=[
             SherlockHomesTheme(theme_name="파란지붕집", theme_id=1023),
             SherlockHomesTheme(theme_name="던전을 부탁해", theme_id=1024),
@@ -209,6 +243,8 @@ SHERLOCK_HOMES_CAFE_LIST: Final = [
     SherlockHomesCafe(
         region1="인천광역시",
         region2="부평점",
+        sido=4,
+        bno=45,
         theme_list=[
             SherlockHomesTheme(theme_name="수상한형제들", theme_id=1029),
             SherlockHomesTheme(theme_name="이상한 나라의 초대 [부평]", theme_id=1030),
@@ -220,6 +256,8 @@ SHERLOCK_HOMES_CAFE_LIST: Final = [
     SherlockHomesCafe(
         region1="충청 대전 세종",
         region2="대전 신세계백화점",
+        sido=12,
+        bno=85,
         theme_list=[
             SherlockHomesTheme(theme_name="지프리트의 심장", theme_id=1154),
             SherlockHomesTheme(theme_name="죽음의 신", theme_id=1155),
@@ -232,6 +270,8 @@ SHERLOCK_HOMES_CAFE_LIST: Final = [
     SherlockHomesCafe(
         region1="충청 대전 세종",
         region2="아산점",
+        sido=12,
+        bno=86,
         theme_list=[
             SherlockHomesTheme(theme_name="노룸", theme_id=1073),
             SherlockHomesTheme(theme_name="악마를 보았다", theme_id=1074),
@@ -243,6 +283,8 @@ SHERLOCK_HOMES_CAFE_LIST: Final = [
     SherlockHomesCafe(
         region1="충청 대전 세종",
         region2="천안1호점",
+        sido=12,
+        bno=14,
         theme_list=[
             SherlockHomesTheme(theme_name="마법사의 세계", theme_id=1096),
             SherlockHomesTheme(theme_name="호텔:사라진 다이아몬드", theme_id=1097),
@@ -256,6 +298,8 @@ SHERLOCK_HOMES_CAFE_LIST: Final = [
     SherlockHomesCafe(
         region1="충청 대전 세종",
         region2="천안2호점",
+        sido=12,
+        bno=74,
         theme_list=[
             SherlockHomesTheme(
                 theme_name="[프리미엄 테마]미즈몰리아와 수수께끼의 책", theme_id=1101
@@ -268,6 +312,8 @@ SHERLOCK_HOMES_CAFE_LIST: Final = [
     SherlockHomesCafe(
         region1="충청 대전 세종",
         region2="청주점",
+        sido=12,
+        bno=70,
         theme_list=[
             SherlockHomesTheme(theme_name="반고흐의방", theme_id=1198),
             SherlockHomesTheme(theme_name="무속인 살인사건", theme_id=1199),
@@ -279,6 +325,8 @@ SHERLOCK_HOMES_CAFE_LIST: Final = [
     SherlockHomesCafe(
         region1="전라 광주",
         region2="여수점",
+        sido=13,
+        bno=44,
         theme_list=[
             SherlockHomesTheme(theme_name="크라임씬", theme_id=1751),
             SherlockHomesTheme(theme_name="이상한 나라의 초대", theme_id=1752),
@@ -292,6 +340,8 @@ SHERLOCK_HOMES_CAFE_LIST: Final = [
     SherlockHomesCafe(
         region1="경상도",
         region2="대구동성로점",
+        sido=15,
+        bno=84,
         theme_list=[
             SherlockHomesTheme(theme_name="지프리트의 심장", theme_id=1286),
             SherlockHomesTheme(theme_name="황금 감옥 : 와캄", theme_id=1287),
--- src/scrapers/sherlockHomes/scrapeSherlockHomes.py
+++ src/scrapers/sherlockHomes/scrapeSherlockHomes.py
@@ -1,78 +1,52 @@
-import time
+import urllib
 from datetime import datetime, timedelta
+from urllib.request import urlopen
 
-from selenium.webdriver.common.by import By
+import bs4
+from bs4 import BeautifulSoup
 
-from src.scrapers.sherlockHomes.constant import SHERLOCK_HOMES_CAFE_LIST, SHERLOCK_URL
-from src.utils.chromeCustomDriver import get_chrome_driver
-from src.utils.database import update_theme_date, make_theme_date
+from src.scrapers.sherlockHomes.constant import SHERLOCK_HOMES_CAFE_LIST, SHERLOCK_HOMES_URL
+from src.utils.database import make_theme_date, update_theme_date
 from src.utils.util import try_except_handling
 
 
-def scrap_sherlock_homes_theme():
-    driver = get_chrome_driver()
-
-    driver.get(SHERLOCK_URL)
-
-    driver.implicitly_wait(3)
-
+def scrape_sherlock_homes_theme():
     theme_date_list = []
     now = datetime.now()
 
-    for cafe in SHERLOCK_HOMES_CAFE_LIST:
-        if click_cafe(driver, cafe) is None:
-            continue
+    for dateDelta in range(7):
+        date = now + timedelta(dateDelta)
+        date_str = date.strftime('%Y-%m-%d')
 
-        for dateDelta in range(7):
-            date = now + timedelta(dateDelta)
-            date_str = date.strftime('%Y-%m-%d')
-            if click_date(driver, date) is None:
-                continue
+        for cafe in SHERLOCK_HOMES_CAFE_LIST:
+            bs_object = get_bs_object(SHERLOCK_HOMES_URL.format(SIDO=cafe.sido, BNO=cafe.bno, DATE=date_str))
 
             for theme in cafe.theme_list:
-                theme_time_result = get_theme_time_result(driver, theme.theme_name)
-                time_list = [element.text for element in theme_time_result]
-
+                time_list = scrape_sherlock_homes_theme_bs4(bs_object, theme.theme_num)
+                print(time_list)
                 theme_date_list = theme_date_list + (make_theme_date(theme.theme_id, date_str, time_list))
-                print(f'{datetime.now()} scraping {date_str} {cafe.region1} {cafe.region2} {theme.theme_name}')
+                print(f'{datetime.now()} scraping {date_str} {cafe.region2} {theme.theme_name}')
 
     theme_id_list = [theme.theme_id for cafe in SHERLOCK_HOMES_CAFE_LIST for theme in cafe.theme_list]
 
     update_theme_date(theme_id_list, theme_date_list)
-    driver.quit()
-    return
+
+
+def get_bs_object(url):
+    html = urllib.request.urlopen(urllib.request.Request(url)).read().decode("utf-8")
+    bs_object = BeautifulSoup(html, "lxml")
+    return bs_object
 
 
 @try_except_handling
-def click_cafe(driver, cafe) -> bool:
-    driver.find_element(by=By.ID, value="selectArea").find_element(by=By.XPATH,
-                                                                   value=f"//option[text()='{cafe.region1}']").click()
-    driver.implicitly_wait(2)
-
-    driver.find_element(by=By.ID, value="selectBranch").find_element(by=By.XPATH,
-                                                                     value=f"option[text()='{cafe.region2}']").click()
-
-    driver.implicitly_wait(2)
-    return True
+def scrape_sherlock_homes_theme_bs4(bs_object: bs4.BeautifulSoup, theme_num: int):
+    return [element.get_text().strip() for element in bs_object.select
+    ('div.inner container > li:nth-child(1) > div.row > div > a > p.time')]
 
 
-@try_except_handling
-def click_date(driver, date) -> bool:
-    driver.find_element(by=By.ID, value="res_date").click()
-
-    driver.implicitly_wait(1)
-    element = driver.find_element(by=By.ID, value="ui-datepicker-div") \
-        .find_element(by=By.XPATH, value=f"table/tbody/tr/td/a[text()='{date.day}']")
-    driver.execute_script("arguments[0].click();", element)
-
-    time.sleep(1)
-    return True
-
-
-def get_theme_time_result(driver, theme_name):
-    return driver.find_element(by=By.XPATH, value=f"//h2[contains(text(),'{theme_name}')]") \
-        .find_elements(by=By.XPATH, value="../div[2]/div/a/p[1]").copy()
-
+#theme_ac_186 > div.row > div > div > p.time
+#theme_ac_186 > div.row > div > a > p.time
+#theme_ac_283 > div.row > div:nth-child(3) > a > p.time
 
 if __name__ == '__main__':
-    scrap_sherlock_homes_theme()
+    scrape_sherlock_homes_theme()
